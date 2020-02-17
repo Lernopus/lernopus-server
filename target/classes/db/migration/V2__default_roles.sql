7b16bc38dea564dfbc0061fe83733bf6db739b78
Lernopus <amernath555@outlook.com>
2020-02-17 20:57:39 +0530

    Removing Unwanted Target Folder (Git Igonring)

--- target/classes/db/migration/V2__default_roles.sql
+++
@@ -1,4 +0,0 @@
-INSERT IGNORE INTO la_learn_role(la_role_name) VALUES('ROLE_USER');
-INSERT IGNORE INTO la_learn_role(la_role_name) VALUES('ROLE_ADMIN');
-INSERT IGNORE INTO la_learn_role(la_role_name) VALUES('ROLE_AUTHOR');
-INSERT IGNORE INTO la_learn_role(la_role_name) VALUES('ROLE_GUEST');
